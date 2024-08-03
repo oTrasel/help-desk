5e626a075101262e0dcc025c79810eb1ceb8780e
oTrasel <trasel.lucas02@gmail.com>
2024-08-03 20:32:00 -0300

    Commit inicial

---
+++ docker/scripts/start.sh
@@ -0,0 +1,18 @@
+
+case "$APP" in
+	'laravel')
+		a2ensite laravel.conf
+		;;
+	*)
+		a2ensite laravel.conf
+		;;
+esac
+
+: ${WWW_UID:=33}
+: ${WWW_GID:=33}
+
+usermod -u $WWW_UID www-data
+groupmod -g $WWW_GID www-data
+
+# Start supervisord and services
+exec /usr/bin/supervisord  -n -c /etc/supervisor/supervisord.conf
